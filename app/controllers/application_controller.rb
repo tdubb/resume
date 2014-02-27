335c8c743e19c5b8f5afc5aa5cb701f656943ccd
Tyler Wasden <twasden@gmail.com>
2014-02-27 10:19:02 -0700

    redirect to host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :wasdeonrails
+
+  def wasdenonrails
+    if request.env["HTTP_HOST"] == "www.wasdenonrails.com"
+      redirect_to "http://radiant-lowlands-4251.herokuapp.com/"
+    end
+  end
 end
