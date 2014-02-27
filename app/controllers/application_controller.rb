271d82d163a45683fae64187c648e71de07fc01c
Tyler Wasden <twasden@gmail.com>
2014-02-27 10:21:12 -0700

    remove redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :wasdeonrails
-
-  def wasdenonrails
-    if request.env["HTTP_HOST"] == "www.wasdenonrails.com"
-      redirect_to "http://radiant-lowlands-4251.herokuapp.com/"
-    end
-  end
 end
